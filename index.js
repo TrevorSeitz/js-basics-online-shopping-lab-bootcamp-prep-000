1c393e8ade34d5dc9b0c189adaad1e01a2e8a3a2
Trevor Seitz <trevinla@gmail.com>
2018-02-18 18:37:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,16 +66,14 @@ function addToCart(item) {
  }
 
  function removeFromCart(item) {
-
-   for (var i = 0; i < cart.length; i++) {
-     if (cart[i].hasOwnProperty(item)) {
-       cart.splice(i, 1);
-      //  console.log(cart);
-     } else {
-       return 'That item is not in your cart.';
-     }
+     for (var i = 0; i < cart.length; i++) {
+       if (cart[i].hasOwnProperty(item)) {
+         cart.splice(i, 1);
+         console.log(cart);
+         return cart;
+       } 
    }
-   return cart;
+     return 'That item is not in your cart.';
  }
 
 function placeOrder(cardNumber) {
