a57827323bfdf4742728a67bd4784a1bd5047174
Trevor Seitz <trevinla@gmail.com>
2018-02-18 18:46:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,15 +66,19 @@ function addToCart(item) {
  }
 
  function removeFromCart(item) {
-   for (var i = 0; i < cart.length; i++) {
-     if (cart[i].hasOwnProperty(item)) {
+    var inCart = false;
+    for (var i = 0; i < cart.length; i++) {
+      if (cart[i].hasOwnProperty(item)) {
+       inCart = true;
        cart.splice(i, 1);
        console.log(cart);
        return cart;
-     }
-   }
-   return 'That item is not in your cart.';
- }
+       }
+    }
+    if (!inCart) {
+     console.log('That item is not in your cart.');
+    }
+  }
 
 function placeOrder(cardNumber) {
   // write your code here
